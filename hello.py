c78aa4978c6ebe1c8728fca848ea653475f2443e
Stefane Brito <stefane_brito@hotmail.com>
2023-06-05 13:35:30 -0300

    Adicionado comentarios e SHEBANG

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello, world!")
+#!/usr/bin/env python3
+#Linha acima, SHIBANG, mensagem enviada para o sistema(consigo forçar uma versão python para ser rodada(boa prática))
+
+print("Hello, world!") #teste-ignore
